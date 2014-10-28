7337343e6a6e69c38310e2ddf79f05be959fd799
TChatzigiannakis <frozencompass@gmail.com>
2014-10-28 18:53:23 +0200

    Added Permutations().

---
+++ EnumerableExtensionsTests/Permutations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using EnumerableExtensions;
+
+namespace EnumerableExtensionsTests
+{
+    [TestFixture]
+    class Permutations
+    {
+        [Test]
+        public void PermutationsSimple()
+        {
+            var original = new[] {1, 2, 3, 4, 4};
+            var permutations = original.Permutations().ToList();
+
+            Assert.AreEqual(1 * 2 * 3 * 4 * 5, permutations.Count);
+            Assert.IsTrue(original.SequenceEqual(permutations.First()));
+            Assert.IsTrue(original.Reverse().SequenceEqual(permutations.Last()));
+        }
+
+        [Test]
+        public void PermutationsEmpty()
+        {
+            var perms = new int[] {}.Permutations();
+            Assert.AreEqual(1, perms.Count());
+        }
+    }
+}
