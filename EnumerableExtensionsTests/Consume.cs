63235c69551c7304bb8244222b52635967733577
TChatzigiannakis <frozencompass@gmail.com>
2015-01-02 13:16:03 +0200

    Added updated license notice (#4).

--- EnumerableExtensionsTests/Consume.cs
+++ EnumerableExtensionsTests/Consume.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/*
+ * EnumerableExtensions
+ * Copyright (C) 2014-2015  Theodoros Chatzigiannakis
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+using System;
 using NUnit.Framework;
 using System.Linq;
 using EnumerableExtensions;
