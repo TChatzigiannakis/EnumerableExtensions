6902b933e99d7c8b93014a79e677f741b39c2f4d
TChatzigiannakis <frozencompass@gmail.com>
2015-01-02 13:08:39 +0200

    Added updated license notice (#1).

--- EnumerableExtensions/Apply.cs
+++ EnumerableExtensions/Apply.cs
@@ -1,8 +1,10 @@
-﻿/* This Source Code Form is subject to the terms of the Mozilla Public
+﻿/*
+ * EnumerableExtensions
+ * Copyright (C) 2014-2015  Theodoros Chatzigiannakis
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- * 
- * Copyright (C) 2014  Theodoros Chatzigiannakis
  */
 
 using System;
