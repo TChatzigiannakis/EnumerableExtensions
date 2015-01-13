bb1f9b16c4f070549cfac3d016c86bc85ff85057
TChatzigiannakis <frozencompass@gmail.com>
2015-01-13 13:23:27 +0200

    Added SkipLast and tests.

--- EnumerableExtensions/SkipLast.cs
+++ EnumerableExtensions/SkipLast.cs
@@ -8,23 +8,24 @@ namespace EnumerableExtensions
     public static partial class EnumerableExtensions
     {
         /// <summary>
-        /// Takes a number of elements from the given sequence, leading up to the last one. If the original sequence doesn't contain enough elements to take, all of its elements are returned. 
+        /// Takes all elements from the sequence, except a count of elements leading up to the last. If the original sequence doesn't have enough elements to skip, an empty sequence is returned.
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="sequence"></param>
         /// <param name="count"></param>
         /// <returns></returns>
-        public static IEnumerable<T> TakeLast<T>(this IEnumerable<T> sequence, int count)
+        public static IEnumerable<T> SkipLast<T>(this IEnumerable<T> sequence, int count)
         {
             if (sequence == null) throw new ArgumentNullException("sequence");
-            if (count <= 0) return new T[0];
+            if (count < 0) throw new ArgumentException("The count can't be a negative number.");
 
             var buffer = new T[count];
             var index = 0;
             var pastFull = false;
-
             foreach (var e in sequence)
             {
+                if (pastFull) 
+                    yield return buffer[index];
                 buffer[index++] = e;
                 if (index == count)
                 {
@@ -32,10 +33,6 @@ namespace EnumerableExtensions
                     pastFull = true;
                 }
             }
-
-            if (!pastFull) return buffer.Take(index);
-
-            return buffer.Skip(index).Concat(buffer.Take(index));
         }
     }
 }
---
+++ EnumerableExtensionsTests/SkipLast.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnumerableExtensions;
+using NUnit.Framework;
+
+namespace EnumerableExtensionsTests
+{
+    [TestFixture]
+    class SkipLast
+    {
+        [Test]
+        public void SkipLastSimple()
+        {
+            var data = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+            var skipped = data.SkipLast(3).ToArray();
+            var skippedTooMany = data.SkipLast(15).ToArray();
+
+            Assert.IsTrue(skipped.SequenceEqual(new[] {1, 2, 3, 4, 5, 6}));
+            Assert.IsFalse(skippedTooMany.Any());
+        }
+    }
+}
